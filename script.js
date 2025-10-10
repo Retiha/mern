792191e907daca02a83ca7e9e08d96d4246955e7
Retiha <retiha.c@gmail.com>
2025-10-10 12:14:25 +0530

    data

---
+++ script.js
@@ -0,0 +1,20 @@
+/*let a=6;
+let b=a;
+*/
+
+
+
+
+
+
+function sum(a,b){
+    console.log(a+b);
+}
+sum(10,11)
+/* used in react function*/
+const sum2=(a,b,c)=>{
+    console.log(a+b+c);
+}
+sum2(10,11,12)
+
+
