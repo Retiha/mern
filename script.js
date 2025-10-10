e298690a50bd93d7830b9ee09d9b7eaf2eff58ac
Retiha <retiha.c@gmail.com>
2025-10-10 14:00:16 +0530

    data

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 /*let a=6;
 let b=a;
+a=4;
+console.log(b);
+
+let obj1={age:33};
+let obj2=obj1;
+obj1.age=44;
+console.log(obj2.age);
 */
-
-
-
-
+let a =true;
+let b =false;
+if(a||b){
+    console.log("hello");
+}
+console.log(9<<2);
+console.log(9|5);
+console.log(9^5);
 
 
 function sum(a,b){
